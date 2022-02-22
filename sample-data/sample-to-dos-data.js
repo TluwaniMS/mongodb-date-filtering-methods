8ced6c481de3065a8a596c0b852a625f30fec1e1
tluwaniMS <tluwani2020@gmail.com>
2022-02-22 10:09:00 +0200

    adding-sample-data

---
+++ sample-data/sample-to-dos-data.js
@@ -0,0 +1,54 @@
+const SampleData = [
+  {
+    title: "Review PR's",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2013, 04, 30)
+  },
+  {
+    title: "Learn Redux",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2015, 07, 19)
+  },
+  {
+    title: "Go Over React Router",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2014, 08, 06)
+  },
+  {
+    title: "Learn Next.JS",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2013, 05, 02)
+  },
+  {
+    title: "Review Keystone",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2022, 03, 14)
+  },
+  {
+    title: "Contribute to Free Code Camp",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2019, 08, 13)
+  },
+  {
+    title: "Learn Cloud Computing",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2021, 05, 18)
+  },
+  {
+    title: "Host Practice Projects",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2017, 05, 24)
+  },
+  {
+    title: "Review remix_run",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2019, 08, 21)
+  },
+  {
+    title: "Start learning Redis Cache",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    date: new Date(2012, 15)
+  }
+];
+
+module.exports = { SampleData };
